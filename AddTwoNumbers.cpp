98722e4b76dc41909177c4d0d61c323c9ecdb675
Shubham Shewale <99116184+shubham-0505@users.noreply.github.com>
2023-10-01 16:59:08 +0530

    Create AddTwoNumbers.cpp

---
+++ AddTwoNumbers.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode *dummy = new ListNode;
+        ListNode *temp = dummy;
+        dummy->next = temp;
+        int carry = 0;
+
+        while(l1!=NULL || l2!=NULL || carry!=NULL){
+            int sum = 0;
+            
+            if(l1!=NULL){
+                sum = sum + l1->val;
+                l1 = l1->next;
+            }
+            if(l2!=NULL){
+                sum = sum + l2->val;
+                l2 = l2->next;
+            }
+
+            sum = sum + carry;
+            carry = (sum/10);
+
+            ListNode *node = new ListNode;
+            node->val = sum%10;
+            temp->next = node;
+            temp = temp->next;
+        }
+        return dummy->next;
+    }
+};
